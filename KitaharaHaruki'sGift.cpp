2671ebe72e03e6e645104497404d95c2729a269f
Howters <87410370+Howters@users.noreply.github.com>
2024-07-29 13:01:33 +0700

    Add files via upload

---
+++ KitaharaHaruki'sGift.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <string.h>
+#define lld  long long int
+#define loop(x,n) for(int x = 0; x < n; x++)
+		
+using namespace std;
+
+int main(){
+	int n;
+	cin >> n;
+	int count1=0, count2=0;
+	loop(i,n){
+		int x;
+		cin >> x;
+		if(x == 100){
+			count1++;
+		}
+		else {
+			count2++;
+		}
+	}
+	if((count1 == 1 && count2 == 0) || (count1==0 && count2==1) || (count1%2==1 && count2 == 0) || (count2%1==1 && count1==0)){
+		cout << "NO" << endl;
+		return 0;
+	}
+	if(count1 %2 ==1){
+		if(count2%2 ==1 ){
+			cout << "NO" << endl;
+		}
+		else{
+			cout << "NO" << endl;
+		}
+	}
+	else{
+		if(count2%2==1){
+			cout << "YES" << endl;
+		}
+		else {
+			cout << "YES" << endl;
+		}
+	}
+//	cout << count1 << " " << count2 << endl;
+
+
+}
