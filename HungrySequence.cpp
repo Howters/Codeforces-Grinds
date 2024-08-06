25b7d48014b95394018fefb6c83c29e2f590550f
Howters <87410370+Howters@users.noreply.github.com>
2024-08-06 13:28:55 +0700

    Add files via upload

---
+++ HungrySequence.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <string.h>
+#include <math.h>
+#define lld  long long int
+#define loop(x,n) for(int x = 0; x < n; x++)
+		
+using namespace std;
+
+
+int main(){
+	lld n;
+	cin >> n;
+	loop(i,n){
+		cout <<  3 * i + n << " ";
+	}
+
+}
